eb4fd4bbff96c040b26449372e51f8831e408eb9
Suleman-Tanveer <sulemantanveer7@gmail.com>
2016-08-02 21:21:04 +0500

    Add files via upload
    This is the .sql file contains the queries for creating tables related to the Search module.

---
+++ searchSideTable.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 02, 2016 at 02:39 PM
+-- Server version: 5.1.54
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `doctify`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `healthcheck`
+--
+
+CREATE TABLE IF NOT EXISTS `healthcheck` (
+  `healthcheck_id` int(11) NOT NULL AUTO_INCREMENT,
+  `healthcheck_category` text NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  PRIMARY KEY (`healthcheck_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `healthcheck`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE IF NOT EXISTS `history` (
+  `history_id` int(11) NOT NULL AUTO_INCREMENT,
+  `time_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `patient_id` int(11) NOT NULL,
+  `doctor_id` varchar(50) NOT NULL,
+  `healthcheck_id` int(11) NOT NULL,
+  `scan_id` int(11) NOT NULL,
+  `appointment_no` int(11) NOT NULL,
+  PRIMARY KEY (`history_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `history`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scan`
+--
+
+CREATE TABLE IF NOT EXISTS `scan` (
+  `scan_id` int(11) NOT NULL AUTO_INCREMENT,
+  `scan_category` text NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  PRIMARY KEY (`scan_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `scan`
+--
+
